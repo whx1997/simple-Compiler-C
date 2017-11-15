05a138b5f0e36620d054992ded3600c7fca22e7a
SteveLee <591657699@qq.com>
2017-11-15 19:39:02 +0800

    Add basic calculation.

--- mylexer.h
+++ mylexer.h
@@ -1,7 +1,7 @@
 /****************************************************************************
 *                     U N R E G I S T E R E D   C O P Y
 * 
-* You are on day 38 of your 30 day trial period.
+* You are on day 32 of your 30 day trial period.
 * 
 * This file was produced by an UNREGISTERED COPY of Parser Generator. It is
 * for evaluation purposes only. If you continue to use Parser Generator 30
@@ -19,7 +19,7 @@
 * C++ header file generated from mylexer.l.
 * 
 * Date: 11/15/17
-* Time: 15:31:40
+* Time: 19:36:57
 * 
 * ALex Version: 2.07
 ****************************************************************************/
